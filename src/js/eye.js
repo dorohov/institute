35bfca47e17144a06ef4c331407da2d8f67cc0c3
Pavel Proyavin <pavel@dorohovdesign.com>
2019-12-10 15:02:40 +0300

    added eye mode

---
+++ src/js/eye.js
@@ -0,0 +1,29 @@
+function setDefaultMode() {
+    $('#eyestyles').remove()
+    localStorage.setItem('mode', 'normal')
+    $('.navbar__top__eye button span').html('Для слабовидящих')
+}
+
+function setEyeMode() {
+    $('head').append('<link rel="stylesheet" href="dist/css/eye.css" id="eyestyles">')
+    localStorage.setItem('mode', 'eye')
+    $('.navbar__top__eye button span').html('Отключить')
+}
+
+function changeMode() {
+    var currentMode = localStorage.getItem('mode')
+
+    if(currentMode == 'normal') {
+        setEyeMode();
+    }else {
+        setDefaultMode();
+    }
+}
+
+var _currentMode = localStorage.getItem('mode')
+
+if(_currentMode == 'normal') {
+    setDefaultMode();
+}else {
+    setEyeMode();
+}
